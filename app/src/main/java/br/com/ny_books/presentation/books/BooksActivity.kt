a7a1140175e38a7b225b334f0855a798584c8a31
igor.bagliotti <igor.bagliotti@invillia.com>
2020-11-11 15:29:51 -0300

    Adicionar estrutura inicial do projeto

---
+++ app/src/main/java/br/com/ny_books/presentation/books/BooksActivity.kt
@@ -0,0 +1,12 @@
+package br.com.ny_books.presentation.books
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.ny_books.R
+
+class BooksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books_main)
+    }
+}
