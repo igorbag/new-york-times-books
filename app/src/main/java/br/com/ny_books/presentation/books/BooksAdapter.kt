5d1e5d1642fd44c68d7540d9ac4050da3a1ba6b4
igor.bagliotti <igor.bagliotti@invillia.com>
2020-11-19 16:42:11 -0300

    Organize code.

--- app/src/main/java/br/com/ny_books/presentation/books/BooksAdapter.kt
+++ app/src/main/java/br/com/ny_books/presentation/books/BooksAdapter.kt
@@ -8,30 +8,31 @@ import br.com.ny_books.R
 import br.com.ny_books.data.model.Book
 import kotlinx.android.synthetic.main.item_book.view.*
 
+
 class BooksAdapter(
-    val books: List<Book>,
-    private val onItemClickListener: (book: Book) -> Unit
+    private val books: List<Book>,
+    private val onItemClickListener: ((book: Book) -> Unit)
 ) : RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
-        val itemView =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, view: Int): BooksViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false)
         return BooksViewHolder(itemView, onItemClickListener)
     }
+
+    override fun getItemCount() = books.count()
 
     override fun onBindViewHolder(viewHolder: BooksViewHolder, position: Int) {
         viewHolder.bindView(books[position])
     }
 
-    override fun getItemCount() = books.count()
-
     class BooksViewHolder(
-        view: View,
-        private val onItemClickListener: (book: Book) -> Unit) :
-        RecyclerView.ViewHolder(view) {
+        itemView: View,
+        private val onItemClickListener: ((book: Book) -> Unit)
+    ) : RecyclerView.ViewHolder(itemView) {
 
         private val title = itemView.textTitle
         private val author = itemView.textAuthor
+
         fun bindView(book: Book) {
             title.text = book.title
             author.text = book.author
